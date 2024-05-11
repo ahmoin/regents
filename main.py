10a9751dedc61ba8931ff6e9a4264bf350718e66
Ahsan Moin <123082059+ahmoin@users.noreply.github.com>
2024-05-11 19:29:45 -0400

    feat: add pdf_link grouping

--- main.py
+++ main.py
@@ -34,11 +34,56 @@ EXCLUDED_HREFS = [
     "http://www.nysed.gov/terms-of-use",
 ]
 
-response = requests.get(MAIN_URL)
-soup = BeautifulSoup(response.text, "html.parser")
-anchor_tags = soup.find_all("a", href=True)
-hrefs = [tag["href"] for tag in anchor_tags if tag["href"] not in EXCLUDED_HREFS]
 
-links = [urljoin(MAIN_URL, href) for href in hrefs]
+def get_valid_input(prompt, max_value):
+    while True:
+        try:
+            index = int(input(prompt))
+            if 1 <= index <= max_value:
+                return index
+            else:
+                print(
+                    "Invalid index. Please enter a number between 1 and %d." % max_value
+                )
+        except ValueError:
+            print("Invalid input. Please enter a number.")
 
-print(links)
+
+def fetch_links_from_url(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+    anchor_tags = soup.find_all("a", href=True)
+    hrefs = [tag["href"] for tag in anchor_tags if tag["href"] not in EXCLUDED_HREFS]
+    return hrefs
+
+
+def main():
+    links = [urljoin(MAIN_URL, href) for href in fetch_links_from_url(MAIN_URL)]
+    for i, link in enumerate(links):
+        print("%d. %s" % (i + 1, link))
+
+    index = get_valid_input(
+        "Enter the index of the link you want to select (1 to %d): " % len(links),
+        len(links),
+    )
+    selected_link = links[index - 1]
+    selected_urls = fetch_links_from_url(selected_link)
+    selected_pdfs = [
+        link.split("/")[-1] for link in selected_urls if link.endswith(".pdf")
+    ]
+    groups = {}
+
+    for _, pdf_link in enumerate(selected_pdfs):
+        identifier = "".join(s for s in pdf_link if s.isdigit())
+
+        if identifier not in groups:
+            groups[identifier] = []
+
+        groups[identifier].append(pdf_link)
+
+    for key, value in groups.items():
+        print(key, ":", value)
+
+
+if __name__ == "__main__":
+    main()
